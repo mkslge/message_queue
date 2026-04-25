4aad3f7a4dd5e34af7d1e5e31026f6db931a204f
mkslge <marklseeliger@gmail.com>
2026-04-25 15:40:33 -0400

    add gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/markseeliger/Desktop/Coding/messagequeue
-# Build directory: /Users/markseeliger/Desktop/Coding/messagequeue/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/markseeliger/Desktop/Coding/messagequeue/build/messagequeue_tests[1]_include.cmake")
